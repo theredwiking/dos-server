60c54f48909ba937df12e7a12b7e4bd28021b2f5
Theredwiking <hjulmandn@gmail.com>
2024-05-22 12:17:34 +0200

    Added firebase

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
+	firebase "firebase.google.com/go/v4"
 	"github.com/theredwiking/dos-server/api"
 )
 
 func main() {
+	_, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("error initializing firebase app: %v\n", err)
+	}
 	router := http.NewServeMux()
 	router.HandleFunc("GET /status", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Server is running"))
